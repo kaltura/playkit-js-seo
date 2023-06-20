d353868e75e2202869d4848df5c3bcd60417e068
JonathanTGold <jonathan.gold@86@gmail.com>
2023-06-20 16:18:39 +0300

    feat(FEC-13119): Add support for SEO

---
+++ src/date-formaters.ts
@@ -0,0 +1,43 @@
+/**
+ * @public
+ * @param {string} durationInSeconds - duration in Seconds
+ * @return {string} - The evaluated string
+ */
+export function convertDurationToISO8601(durationInSeconds: number): string {
+  const seconds = Math.floor(durationInSeconds % 60);
+  const minutes = Math.floor((durationInSeconds / 60) % 60);
+  const hours = Math.floor((durationInSeconds / (60 * 60)) % 24);
+  const days = Math.floor(durationInSeconds / (60 * 60 * 24));
+
+  const paddedHours = hours.toString().padStart(2, '0');
+  const paddedMinutes = minutes.toString().padStart(2, '0');
+
+  const isoDuration = `P${days || ''}DT${paddedHours}H${paddedMinutes}M${seconds}S`;
+  return isoDuration;
+}
+
+/**
+ * @public
+ * @param {string} unixTimestamp - date in Unix timestamp format
+ * @return {string} - The evaluated string in ISO 8601 format without the UTC offset
+ */
+export function convertUnixTimestampToISO8601(unixTimestamp: number): string {
+  const milliseconds = unixTimestamp * 1000;
+
+  // Create a new Date object with the provided Unix timestamp
+  const date = new Date(milliseconds);
+
+  // Get the current timezone offset in minutes
+  const timezoneOffset = date.getTimezoneOffset();
+
+  // Calculate the new time value by subtracting the timezone offset
+  const newTimeValue = date.getTime() - (timezoneOffset * 60 * 1000);
+
+  // Create a new Date object with the updated time value
+  const newDate = new Date(newTimeValue);
+
+  // Convert the new date to ISO 8601 format
+  const iso8601Date = newDate.toISOString().replace('.000Z', '+00:00');
+
+  return iso8601Date;
+}
--- src/seo.ts
+++ src/seo.ts
@@ -1,4 +1,9 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+
 import { BasePlugin } from '@playkit-js/kaltura-player-js';
+import { convertUnixTimestampToISO8601, convertDurationToISO8601 } from './date-formaters';
+import type { VideoObject, WithContext } from 'schema-dts';
 
 export const pluginName = 'seo';
 
@@ -10,11 +15,36 @@ export class Seo extends BasePlugin<Record<string, unknown>> {
   }
 
   protected loadMedia(): void {
-    this.todo();
+    if (this.hasStructuredDataRequiredProperties()) {
+      const SEOStructuredData: WithContext<VideoObject> = this.getSEOStructuredData();
+      Seo.sendSEOStructuredData(SEOStructuredData);
+    } else {
+      this.logger.debug('SEO Structured Data Required properties are missing');
+    }
   }
 
-  private todo(): void {
-    // eslint-disable-next-line no-console
-    console.log('`HI this is SEO...');
+  private getSEOStructuredData(): WithContext<VideoObject> {
+    const VideoStructuredData: WithContext<VideoObject> = {
+      '@context': 'https://schema.org',
+      '@type': 'VideoObject',
+      name: this.player.sources.metadata.name,
+      description: this.player.sources.metadata.description,
+      thumbnailUrl: this.player.sources.poster,
+      uploadDate: convertUnixTimestampToISO8601(this.player.sources.metadata.createdAt),
+      duration: convertDurationToISO8601(this.player.sources.duration!),
+      contentUrl: this.player.selectedSource.url
+    };
+    return VideoStructuredData;
+  }
+
+  private static sendSEOStructuredData(SEOStructuredData: WithContext<VideoObject>): void {
+    window.parent.postMessage({ type: 'SEOStructuredData', SEOStructuredData }, '*');
+  }
+
+  private hasStructuredDataRequiredProperties(): boolean {
+    const name = this.player.sources.metadata.name;
+    const thumbnailUrl = this.player.sources.poster;
+    const createdAt = this.player.sources.metadata.createdAt;
+    return name && thumbnailUrl && createdAt;
   }
 }
