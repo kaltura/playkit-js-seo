cf4c36d40072741800caa0189daf3f65ce0f5f72
Tzipi-kaltura <tzipi.maksev@kaltura.com>
2026-03-02 11:13:51 +0200

    fix lint

--- src/providers/utils/request-builder-helper.ts
+++ src/providers/utils/request-builder-helper.ts
@@ -1,11 +1,6 @@
 import { RequestBuilder } from '@playkit-js/playkit-js-providers/ovp-provider';
 
-export function buildRequests(
-  items: any[],
-  service: string,
-  action: string,
-  paramKey: string = 'id'
-): RequestBuilder[] {
+export function buildRequests(items: any[], service: string, action: string, paramKey: string): RequestBuilder[] {
   const headers: Map<string, string> = new Map();
   return items.map((item: { id: string }) => {
     const request = new RequestBuilder(headers);
